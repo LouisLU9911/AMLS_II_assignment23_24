0587c9a9b6e568c11f9ff1ec3b74dce4655bdc36
LouisLU9911 <louislu9911@gmail.com>
2024-03-25 23:29:27 +0000

    feat: train different models

---
+++ train_resnet_101.py
@@ -0,0 +1,141 @@
+import os
+
+from datasets import load_dataset, load_metric
+from transformers import (
+    AutoImageProcessor,
+    AutoModelForImageClassification,
+    TrainingArguments,
+    Trainer,
+)
+import numpy as np
+import torch
+from torchvision.transforms import (
+    CenterCrop,
+    Compose,
+    Normalize,
+    RandomHorizontalFlip,
+    RandomResizedCrop,
+    Resize,
+    ToTensor,
+)
+
+BATCH_SIZE = 120
+
+cwd = os.getcwd()
+train_image_folder = os.path.join(cwd, "Datasets", "imagefolder")
+
+print(f"begin loading {train_image_folder} ...")
+dataset = load_dataset("imagefolder", data_dir=train_image_folder)
+print("dataset setup successfully!")
+model_checkpoint = "microsoft/resnet-101"
+
+image_processor = AutoImageProcessor.from_pretrained(model_checkpoint)
+normalize = Normalize(mean=image_processor.image_mean, std=image_processor.image_std)
+if "height" in image_processor.size:
+    size = (image_processor.size["height"], image_processor.size["width"])
+    crop_size = size
+    max_size = None
+elif "shortest_edge" in image_processor.size:
+    size = image_processor.size["shortest_edge"]
+    crop_size = (size, size)
+    max_size = image_processor.size.get("longest_edge")
+train_transforms = Compose(
+    [
+        RandomResizedCrop(crop_size),
+        RandomHorizontalFlip(),
+        ToTensor(),
+        normalize,
+    ]
+)
+
+val_transforms = Compose(
+    [
+        Resize(size),
+        CenterCrop(crop_size),
+        ToTensor(),
+        normalize,
+    ]
+)
+
+
+def preprocess_train(example_batch):
+    """Apply train_transforms across a batch."""
+    example_batch["pixel_values"] = [
+        train_transforms(image.convert("RGB")) for image in example_batch["image"]
+    ]
+    return example_batch
+
+
+def preprocess_val(example_batch):
+    """Apply val_transforms across a batch."""
+    example_batch["pixel_values"] = [
+        val_transforms(image.convert("RGB")) for image in example_batch["image"]
+    ]
+    return example_batch
+
+
+# split up training into training + validation
+splits = dataset["train"].train_test_split(test_size=0.1)
+train_ds = splits["train"]
+val_ds = splits["test"]
+train_ds.set_transform(preprocess_train)
+val_ds.set_transform(preprocess_val)
+
+model = AutoModelForImageClassification.from_pretrained(
+    model_checkpoint,
+    # token = access_token,
+)
+model_name = model_checkpoint.split("/")[-1]
+
+args = TrainingArguments(
+    f"{model_name}-finetuned-cassava-leaf-disease",
+    remove_unused_columns=False,
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    learning_rate=5e-5,
+    per_device_train_batch_size=BATCH_SIZE,
+    gradient_accumulation_steps=4,
+    dataloader_num_workers=5,
+    per_device_eval_batch_size=BATCH_SIZE,
+    num_train_epochs=30,
+    warmup_ratio=0.1,
+    logging_steps=10,
+    load_best_model_at_end=True,
+    metric_for_best_model="accuracy",
+    push_to_hub=True,
+)
+
+metric = load_metric("accuracy")
+
+
+# the compute_metrics function takes a Named Tuple as input:
+# predictions, which are the logits of the model as Numpy arrays,
+# and label_ids, which are the ground-truth labels as Numpy arrays.
+def compute_metrics(eval_pred):
+    """Computes accuracy on a batch of predictions"""
+    predictions = np.argmax(eval_pred.predictions, axis=1)
+    return metric.compute(predictions=predictions, references=eval_pred.label_ids)
+
+
+def collate_fn(examples):
+    pixel_values = torch.stack([example["pixel_values"] for example in examples])
+    labels = torch.tensor([example["label"] for example in examples])
+    return {"pixel_values": pixel_values, "labels": labels}
+
+
+trainer = Trainer(
+    model,
+    args,
+    train_dataset=train_ds,
+    eval_dataset=val_ds,
+    compute_metrics=compute_metrics,
+    data_collator=collate_fn,
+)
+print("Begin training...")
+train_results = trainer.train()
+print("training ends")
+# rest is optional but nice to have
+trainer.save_model()
+trainer.log_metrics("train", train_results.metrics)
+trainer.save_metrics("train", train_results.metrics)
+trainer.save_state()
